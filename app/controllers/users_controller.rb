eb7138fe3a49384dd181498dbcc2379dfa27d256
bellamysmith <bellamysmith1@gmail.com>
2014-10-17 15:10:03 -0700

    init

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+require 'twilio-ruby'
+require 'unirest'
+
+
+  def index
+  end
+
+  def create
+@response = Unirest.get "http://catfacts-api.appspot.com/api/facts"
+
+	#puts response.body, response.code, response.message, response.headers.inspect
+
+
+  	@user = User.new(phone: params[:user][:phone]);
+  	if @user.save
+ 		account_sid = ENV['TWILIO_KEY']
+		auth_token = ENV['TWILIO_AUTH'] 
+ 
+# set up a client to talk to the Twilio REST API 
+		@client = Twilio::REST::Client.new account_sid, auth_token 
+ 
+		@client.account.messages.create({
+			:from => '+16193762984', 
+			:to => @user.phone, 
+			:body => params[:user][:text] + " " + @response.body["facts"][0],  
+		})
+		redirect_to root_path
+	end
+  end
+
+  def new
+  end
+end
+
---
+++ db/migrate/20141017200041_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
